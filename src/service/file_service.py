fc04135d67edeb540ef4312a94dc0df53b6d58cf
yanshanlangren <elviszhai@yunify.com>
2023-03-15 14:42:55 +0800

    Update file_service.py

--- src/service/file_service.py
+++ src/service/file_service.py
@@ -1,10 +1,12 @@
 import src.third.open_ai as openai
+import json
 
 
 def file_upload(audio_file):
     audio_file.save("./record.webm")
     trans = open("./record.webm", "rb")
     resp = openai.audio_transcribe(trans)
+    print("audio transcriptions response: %s" % json.dumps(resp, ensure_ascii=False))
     if not resp:
         return None
     return resp.get("text")
